d6167e247f5635c015261505c0d059ed2ce4621f
Raffi <raffi.gukasyan777@gmail.com>
2024-12-09 14:20:23 +0300

    feat(player): finish edits progressbar range

--- src/entities/Player/ui/ProgressBar.tsx
+++ src/entities/Player/ui/ProgressBar.tsx
@@ -1,7 +1,9 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {motion} from "framer-motion";
 export const ProgressBar = ():JSX.Element => {
     const [isProcces, setIsProcces] = useState<boolean>();
+    const [cordinate, setCordinate] = useState<number>(0);
+    const [width, setWidth] = useState<number>(0);
     const progressBarRef = useRef<HTMLDivElement>(null);
     const progressRef = useRef<HTMLDivElement>(null);
 
@@ -9,24 +11,52 @@ export const ProgressBar = ():JSX.Element => {
         return Math.floor((wElement / wParent) * 100);
     }
 
-    return (
-        <div ref={progressBarRef} onMouseDown={() => {
-            setIsProcces(true);
-            console.log(progressBarRef.current?.getClientRects(), 'DOWN')
-        }} onMouseUp={(eve:React.MouseEvent) => {
+    const applyConstraints = (containerWidth:number, draggableWidth:number):number => {
+        if(draggableWidth < 0) {
+            return 0
+        }
+        else if(draggableWidth > containerWidth) {
+            return containerWidth;
+        }
+        return draggableWidth
+    }
+    useEffect(() => {
+        const listener = () => {
             setIsProcces(false);
-            // console.log(progressRef.current?.clientWidth, 'width element');
-            // console.log(progressBarRef.current?.clientWidth, 'width parent');
-            progressRef.current.style.width = getToProcent(progressBarRef.current?.clientWidth as number, eve.nativeEvent.offsetX) + '%';
-        }} onMouseMove={(eve) => {
-            if(isProcces) {
-                progressRef.current.style.width = getToProcent(progressBarRef.current?.clientWidth as number, eve.nativeEvent.offsetX) + '%';
-            } }} onMouseLeave={(e) => setIsProcces(false)} className="w-full bg-gray-200  rounded-xl h-4  dark:bg-gray-700 cursor-pointer">
+        }
+
+        document.body.addEventListener('pointerup', listener);
+        return () => document.removeEventListener('pointerup', listener)
+    }, []);
+
+    useEffect(() => {
+
+        const pointerListener = (eve:PointerEvent) => {
+            if(progressBarRef.current && progressRef.current) {
+                const translate:number =  width + (eve.clientX - cordinate)
+                progressRef.current.style.width = getToProcent(progressBarRef.current?.clientWidth as number, applyConstraints(progressBarRef.current?.clientWidth as number, translate)) + '%';
+            }
+        }
+        if(isProcces) {
+            document.body.addEventListener('pointermove', pointerListener)
+            return ():void => {
+                document.body.removeEventListener('pointermove', pointerListener)
+            }
+        }
+    }, [isProcces]);
+
+    return (
+        <div ref={progressBarRef} onMouseDown={(event) => {
+            setIsProcces(true);
+            setCordinate(event.clientX)
+            setWidth(event.nativeEvent.offsetX)
+            progressRef.current.style.width = getToProcent(progressBarRef.current?.clientWidth as number, event.nativeEvent.offsetX) + '%';
+        }}   className="w-full bg-gray-200  rounded-xl h-2.5  dark:bg-gray-700 cursor-pointer">
             <motion.div  animate={{
                 transition: {
                     width: { ease: "linear", duration: 1 }
                 }
-            }} ref={progressRef}  className="bg-green-600 rounded-xl w-0 h-4 dark:bg-green-500"></motion.div>
+            }} ref={progressRef}  className="bg-green-600 rounded-xl w-0 h-2.5 dark:bg-green-500"></motion.div>
         </div>
     )
 }
